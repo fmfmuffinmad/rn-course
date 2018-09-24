af3dcd75b6a1b9a12a38eb56330bc2976f669a5f
arthur.ferreira <arthur.ferreira@everymind.com.br>
2018-09-23 21:38:42 -0300

    Added and conected Redux

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { StyleSheet, ScrollView, FlatList } from "react-native";
+import { StyleSheet, FlatList } from "react-native";
 
 import ListItem from "../ListItem/ListItem";
 
 const placeList = props => {
-
-  return <FlatList
-    style={styles.listContainer}
-    data={props.places}
-    renderItem={(info) => (
-      <ListItem
-        placeName={info.item.name}
-        placeImage={info.item.image}
-        onItemPressed={() => props.onItemDeleted(info.item.key)}
-      />
-    )}
-  />
+  return (
+    <FlatList
+      style={styles.listContainer}
+      data={props.places}
+      renderItem={(info) => (
+        <ListItem
+          placeName={info.item.name}
+          placeImage={info.item.image}
+          onItemPressed={() => props.onItemSelected(info.item.key)}
+        />
+      )}
+    />
+  );
 };
 
 const styles = StyleSheet.create({

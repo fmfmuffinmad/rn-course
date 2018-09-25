354d0c1acdfe75985f74be075c2357ba6fe20e7e
Arthur  Ferreira <arthur.ferreira@everymind.com.br>
2018-09-25 11:22:32 -0300

    added screens config

---
+++ src/screens/MainTabs/startMainTabs.js
@@ -0,0 +1,28 @@
+import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const startTabs = () => {
+  Promise.all([
+    Icon.getImageSource("md-map", 30),
+    Icon.getImageSource("ios-share-alt", 30)
+  ]).then(sources => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: "awesome-places.FindPlaceScreen",
+          label: "FindPlace",
+          title: "FindPlace",
+          icon: sources[0]
+        },
+        {
+          screen: "awesome-places.SharePlaceScreen",
+          label: "SharePlace",
+          title: "SharePlace",
+          icon: sources[1]
+        }
+      ]
+    });
+  });
+};
+
+export default startTabs;
